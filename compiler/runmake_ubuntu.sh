78180ec1b68666b37cfafb8751d3da8262ff87ec
neeeekitos <nikita.a.terekhov@gmail.com>
2022-06-13 17:31:52 +0200

    test: x86

--- compiler/runmake_ubuntu.sh
+++ compiler/runmake_ubuntu.sh
@@ -2,4 +2,4 @@
 #   sudo apt install     antlr4 libantlr4-runtime-dev default-jdk
 # the ANTLRJAR part below is copied from /usr/bin/antlr4
 rm -r build generated
-make ANTLRJAR=/usr/share/java/stringtemplate4.jar:/usr/share/java/antlr4.jar:/usr/share/java/antlr4-runtime.jar:/usr/share/java/antlr3-runtime.jar/:/usr/share/java/treelayout.jar ANTLRINC=/usr/include/antlr4-runtime ANTLRLIB=/usr/lib/aarch64-linux-gnu/libantlr4-runtime.a "$@"
+make ANTLRJAR=/usr/share/java/stringtemplate4.jar:/usr/share/java/antlr4.jar:/usr/share/java/antlr4-runtime.jar:/usr/share/java/antlr3-runtime.jar/:/usr/share/java/treelayout.jar ANTLRINC=/usr/include/antlr4-runtime ANTLRLIB=/usr/lib/x86_64-linux-gnu/libantlr4-runtime.a "$@"
