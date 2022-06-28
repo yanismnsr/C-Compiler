947095c11d49f137142c51b6207f6f2fdd34b4a2
LouisePietropaoli <louise.pietropaoli@gmail.com>
2022-06-28 21:18:17 +0200

    update visitor

--- compiler/CodeGenVisitor.cpp
+++ compiler/CodeGenVisitor.cpp
@@ -355,7 +355,6 @@ std::any CodeGenVisitor::visitParenthesis(ifccParser::ParenthesisContext *ctx)
 
 std::any CodeGenVisitor::visitUnaryExpression(ifccParser::UnaryExpressionContext *ctx)
 {
-
 	SymbolTable *symbolTable = this->getSymbolTableOfCurrentBlock();
 
 	string oper = ctx->op->getText();
@@ -369,7 +368,24 @@ std::any CodeGenVisitor::visitUnaryExpression(ifccParser::UnaryExpressionContext
 
 	// int variableAddress = variableToMemoryMap[exprVarName] * -4;
 
-	if (oper == "-")
+	if (oper == "!")
+	{
+
+		Type *intType = PrimitiveType::getInstance()->getType("int");
+		bb->add_IRInstr(IRInstr::Operation::unary_not, intType, {temporarySymbolAdded.symbolName, exprVarName});
+
+		return (string)temporarySymbolAdded.symbolName;
+	}
+	else if (oper == "!-")
+	{
+		Type *intType = PrimitiveType::getInstance()->getType("int");
+		bb->add_IRInstr(IRInstr::Operation::sub, intType, {temporarySymbolAdded.symbolName, "0", exprVarName});
+		Symbol otherTemporarySymbolAdded = symbolTable->addTemporaryVariable();
+		bb->add_IRInstr(IRInstr::Operation::unary_not, intType, {otherTemporarySymbolAdded.symbolName, temporarySymbolAdded.symbolName});
+
+		return (string)otherTemporarySymbolAdded.symbolName;
+	}
+	else if (oper == "-")
 	{
 
 		Type *intType = PrimitiveType::getInstance()->getType("int");
